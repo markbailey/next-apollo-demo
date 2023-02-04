e98e493d58a8d571941ac2da279c91801eedae9e
Mark Bailey <markbailey78@yahoo.co.uk>
2023-02-04 19:06:00 +0000

    Update schema.ts
    Create createContact function and moved gender property

--- client/schema.ts
+++ client/schema.ts
@@ -1,9 +1,10 @@
 import casual from 'casual';
 
-casual.define('contact', () => ({
+export const createContact = () => ({
   full_name: casual.full_name,
   forename: casual.first_name,
   surname: casual.last_name,
+  gender: casual.random_value(['Male', 'Female']),
   address: casual.address,
   city: casual.city,
   state: casual.state,
@@ -12,7 +13,9 @@ casual.define('contact', () => ({
   email: casual.email,
   phone: casual.phone,
   bio: casual.words(10)
-}));
+});
+
+casual.define('contact', createContact);
 
 export const typeDefs = `#graphql
   type Contact {
@@ -40,9 +43,9 @@ export const resolvers = {
   Query: {
     name: () => casual.full_name,
     contacts: (root: unknown, args: ContactsArgs) =>
-      Array.from({ length: 2000 }, () => ({
-        ...casual.contact,
-        gender: casual.random_value(['Male', 'Female']) as string
-      })).slice(args.skip, args.skip + args.first)
+      Array.from({ length: 2000 }, () => casual.contact).slice(
+        args.skip,
+        args.skip + args.first
+      )
   }
 };
