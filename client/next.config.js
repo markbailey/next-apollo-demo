d3f54bc501a1513f1bd4b0d9652fd7b659be21b4
Mark Bailey <markbailey78@yahoo.co.uk>
2023-02-04 00:37:56 +0000

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     newNextLinkBehavior: false
+  },
+  images: {
+    deviceSizes: [320, 425, 475, 768, 992, 1200, 1400],
+    imageSizes: [360, 425, 490, 450, 400, 423, 413],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+        port: '',
+        pathname: '/**'
+      }
+    ]
   }
 };
 
