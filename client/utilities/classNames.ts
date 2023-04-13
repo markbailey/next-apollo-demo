a1ee2db70251dac5ac6531a31c7cda8b0a812248
Mark Bailey <markbailey78@yahoo.co.uk>
2023-04-13 13:09:04 +0100

    Update classNames.ts

--- client/utilities/classNames.ts
+++ client/utilities/classNames.ts
@@ -1,7 +1,8 @@
 function classNames<T>(...args: T[]) {
   return args
     .reduce((className: string, value) => {
-      if (typeof value === 'string' && value.trim() !== '') return `${className} ${value.trim()}`;
+      if (typeof value === 'string' && value.trim() !== '')
+        return `${className} ${value.trim()}`;
       return className;
     }, '')
     .trim()
